dc4c7ca6d5be472272fe4edf63f500bdbb49d465
Norbert Janik <59235777+QuantumEspresso@users.noreply.github.com>
2022-12-01 17:13:48 +0000

    Add files via upload

---
+++ zad1.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+awk 'BEGIN{currScore=0;bestScore=0;currElf=0;bestElf=0;wasPrevBlanc=0}
+{
+    if (NF==0)
+    {
+        if (wasPrevBlanc)
+            currElf++;
+        if (currScore>bestScore)
+        {
+            bestScore=currScore;
+            bestElf=currElf;
+        }
+        currScore=0;
+        wasPrevBlanc=0;
+    }
+    else
+    {
+        currScore+=$1;
+        wasPrevBlanc=1;
+    }
+
+}
+END{
+    if (currScore>bestScore)
+    {
+        bestScore=currScore;
+        bestElf=currElf;
+    }
+    print "The best Elf is number " bestElf " with " bestScore " calories.";
+}' $1
